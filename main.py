5ffb90d8aa28433ab32b7ce2fbb6756eceb5fdff
AlinaUK27 <126779111+AlinaUK27@users.noreply.github.com>
2023-03-03 07:56:13 +0100

    "Del list "пекарня" from shopping_dict"

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ shopping_dict = {"пекарня":['хліб', 'булочки', 'пончик']
 "продуктовий": ['морква', 'селера', 'рукола']
 }
 shopping_dict ["господарчий"] = ["відро", "швабра"]
+del shopping_dict ["пекарня"]
+
 for keys, values in shopping_dict.items():
     print(f"Заходжу в {keys.title()}, купую тут такі товари:{values.__str__().title()}")
   
-value1 = shopping_dict["пекарня"]
 value2 = shopping_dict["продуктовий"]
 value3 = shopping_dict["господарчий"]
-print(f"Разом купую {len(value1) + len(value2) + len(value3)} товарів")
+print(f"Разом купую {len(value2) + len(value3)} товарів")
 
 
