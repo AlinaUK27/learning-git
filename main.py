08377c442ca086fe658a21155be4e36e2f51d22c
AlinaUK27 <126779111+AlinaUK27@users.noreply.github.com>
2023-03-03 07:43:54 +0100

    "Add list in shopping_dict "

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ print("Cписок покупок")
 shopping_dict = {"пекарня":['хліб', 'булочки', 'пончик'],
 "продуктовий": ['морква', 'селера', 'рукола']
 }
- 
+shopping_dict ["господарчий"] = ["відро", "швабра"]
 for keys, values in shopping_dict.items():
     print(f"Заходжу в {keys.title()}, купую тут такі товари:{values.__str__().title()}")
+  
 value1 = shopping_dict["пекарня"]
 value2 = shopping_dict["продуктовий"]
-print(f"Разом купую {len(value1) + len(value2)} товарів")
+value3 = shopping_dict["господарчий"]
+print(f"Разом купую {len(value1) + len(value2) + len(value3)} товарів")
+
+
