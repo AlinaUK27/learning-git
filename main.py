00e05e8a6d925347c76f426291a57ccdb7df0000
AlinaUK27 <126779111+AlinaUK27@users.noreply.github.com>
2023-03-03 08:11:49 +0100

    "Update shopping_dict"

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ shopping_dict = {"пекарня":['хліб', 'булочки', 'пончик']
 }
 shopping_dict ["господарчий"] = ["відро", "швабра"]
 del shopping_dict ["пекарня"]
+new_shopping_dict = {"магазин одягу":["сукня", "сумочка"]}
+shopping_dict.update(new_shopping_dict)
 
 for keys, values in shopping_dict.items():
     print(f"Заходжу в {keys.title()}, купую тут такі товари:{values.__str__().title()}")
   
 value2 = shopping_dict["продуктовий"]
 value3 = shopping_dict["господарчий"]
-print(f"Разом купую {len(value2) + len(value3)} товарів")
-
-
+value4 = new_shopping_dict["магазин одягу"]
+print(f"Разом купую {len(value2) + len(value3) + len(value4)} товарів")
